1a79d5bd9bb9e9476af4e8774b4130a1f4c99230
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2021-09-25 22:22:21 +0900

    2021-09-25
    2021-09-25

--- Java/com.test.algorithm/src/com/test/algorithm/TestMain.java
+++ Java/com.test.algorithm/src/com/test/algorithm/TestMain.java
@@ -1,11 +1,31 @@
 package com.test.algorithm;
 
+import java.security.MessageDigest;
+
 public class TestMain {
 	
 	public static void main(String[] args) {
-	
-		System.out.println("asdf");
 		
-		
+		try{
+			
+			String pw = "1234&^5&";
+			
+			MessageDigest digest = MessageDigest.getInstance("SHA-256");
+			byte[] hash = digest.digest(pw.getBytes("UTF-8"));
+			StringBuffer hexString = new StringBuffer();
+
+			for (int i = 0; i < hash.length; i++) {
+				String hex = Integer.toHexString(0xff & hash[i]);
+				if(hex.length() == 1) hexString.append('0');
+				hexString.append(hex);
+			}
+
+			//출력
+			System.out.println(hexString.toString());
+			
+		} catch(Exception ex){
+			throw new RuntimeException(ex);
+		}
+
 	}
 }
