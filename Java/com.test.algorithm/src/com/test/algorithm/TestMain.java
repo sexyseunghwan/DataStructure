b1c19385819f891cdc7f9b4a626b2c0ee4fd2f32
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2021-10-17 00:15:59 +0900

    2021-10-17
    2021-10-17

--- Java/com.test.algorithm/src/com/test/algorithm/TestMain.java
+++ Java/com.test.algorithm/src/com/test/algorithm/TestMain.java
@@ -1,32 +1,64 @@
 package com.test.algorithm;
 
-import java.security.MessageDigest;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class TestMain {
 	
 	public static void main(String[] args) {
 		
-		try{
-			
-			String pw = "1234&^5&";
-			
-			MessageDigest digest = MessageDigest.getInstance("SHA-256");
-			byte[] hash = digest.digest(pw.getBytes("UTF-8"));
-			StringBuffer hexString = new StringBuffer();
+		Product p1 = new Product("갤럭시", 12000000);
+		Product p2 = new Product("오포", 10000000);
+		Product p3 = new Product("아이폰", 16000000);
+		Product p4 = new Product("샤오미", 8000000);
+		
+		
+		ArrayList<Product> arr = new ArrayList<Product>();
+		arr.add(p1);
+		arr.add(p2);
+		arr.add(p3);
+		arr.add(p4);
+		
+		Collections.sort(arr, new ListCompare());
+		
+		for (Product pd : arr) {
+			System.out.println(pd.name);
+			System.out.println(pd.price);
+		}
+		
+		
+	}
+	
+	static class ListCompare implements Comparator<Product> {
 
-			for (int i = 0; i < hash.length; i++) {
-				String hex = Integer.toHexString(0xff & hash[i]);
-				if(hex.length() == 1) hexString.append('0');
-				hexString.append(hex);
-			}
-				
+		@Override
+		public int compare(Product p1, Product p2) {
 			
-			//출력
-			System.out.println(hexString.toString());
+			int productValue1 = p1.price;
+			int productValue2 = p2.price;
 			
-		} catch(Exception ex){
-			throw new RuntimeException(ex);
+			if (productValue1 > productValue2) return -1;
+			else if (productValue1 < productValue2) return 1;
+			else return 0;
+		}
+	
+	}
+	
+	static class Product implements Comparator {
+		public String name;
+		public int price;
+		
+		public Product(String name,int price)  {
+			this.name = name;
+			this.price = price;
 		}
 
+		@Override
+		public int compare(Object o1, Object o2) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
 	}
+	
 }
