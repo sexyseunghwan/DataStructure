278b2ca5ffabd3fea65c9ab8ef4d84bb6b01828d
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2022-02-04 14:29:29 +0900

    2022-02-04
    2022-02-04

--- Java/com.test.algorithm/src/com/test/algorithm/TestTestTest.java
+++ Java/com.test.algorithm/src/com/test/algorithm/TestTestTest.java
@@ -8,29 +8,7 @@ public class TestTestTest {
 	public static void main(String[] args) {
 		
 		
-		int[] arr = new int[4];
 		
-		for (int i = 0; i < 4;i++) {
-			arr[i] = i+34;
-		}
-		
-		
-		
-		int[] test = arr;
-		
-		
-		for (int i = 0; i < 4; i++) {
-			System.out.println(test[i]);	
-		}
-		
-		
-		System.out.println("==================");
-		
-		test[0] = 9300;
-		
-		for (int i = 0; i < 4; i++) {
-			System.out.println(arr[i]);	
-		}
 		
 		
 		
