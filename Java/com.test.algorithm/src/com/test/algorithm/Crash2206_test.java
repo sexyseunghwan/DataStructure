ea84e9d90e868dbeb221c70db33af444c2c4fde1
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2022-02-08 20:41:00 +0900

    2022-02-08
    2022-02-08

--- Java/com.test.algorithm/src/com/test/algorithm/Crash2206_test.java
+++ Java/com.test.algorithm/src/com/test/algorithm/Crash2206_test.java
@@ -1,11 +1,7 @@
 package com.test.algorithm;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.io.*;
+import java.util.*;
 
 public class Crash2206_test {
 	
