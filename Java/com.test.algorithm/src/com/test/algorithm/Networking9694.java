784339a63cf2c0ed1eaf0640f997087ee46448f9
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2022-04-13 23:36:18 +0900

    2022-04-13
    2022-04-13

--- Java/com.test.algorithm/src/com/test/algorithm/Networking9694.java
+++ Java/com.test.algorithm/src/com/test/algorithm/Networking9694.java
@@ -1,17 +1,12 @@
 package com.test.algorithm;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.util.Stack;
-
+import java.io.*;
+import java.util.*;
 
 public class Networking9694 {
 	
 	static ArrayList<ArrayList<Node>> map;
-	static int[] visit;
+	static int[] answer;
 	static int[] prev;
 	static int T,N,M;
 	static int INF = Integer.MAX_VALUE;
@@ -32,27 +27,30 @@ public class Networking9694 {
 		T = Integer.parseInt(br.readLine());
 		
 		while(T-- > 0) {
-			String[] inputs = br.readLine().split(" ");
+			//String[] inputs = br.readLine().split(" ");
 			
-			N = Integer.parseInt(inputs[0]);//관계의 개수
-			M = Integer.parseInt(inputs[1]);//정치인의 수
+			//N = Integer.parseInt(inputs[0]);//관계의 개수
+			//M = Integer.parseInt(inputs[1]);//정치인의 수
+			StringTokenizer stk = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(stk.nextToken());//관계의 개수
+			M = Integer.parseInt(stk.nextToken());//정치인의 수
+			
 			
 			prev = new int[M];
-			visit = new int[M];
+			answer = new int[M];
 			map = new ArrayList<ArrayList<Node>>();
 			
 			clean();
 			
 			for (int i = 0; i < M; i++) {
-				ArrayList<Node> arr = new ArrayList<Node>();
-				map.add(arr);
+				map.add(new ArrayList<>());
 			}
 			
 			for (int i = 0; i < N; i++) {
-				String[] elements = br.readLine().split(" ");
-				int r = Integer.parseInt(elements[0]);
-				int c = Integer.parseInt(elements[1]);
-				int w = Integer.parseInt(elements[2]);
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				int r = Integer.parseInt(st.nextToken());
+				int c = Integer.parseInt(st.nextToken());
+				int w = Integer.parseInt(st.nextToken());
 				
 				map.get(r).add(new Node(c,w));
 				map.get(c).add(new Node(r,w));
@@ -61,10 +59,6 @@ public class Networking9694 {
 			dijkstra();
 			
 		}//while
-		
-
-	
-		
 	}//main()
 	
 	static void dijkstra() {
@@ -79,7 +73,7 @@ public class Networking9694 {
 			}
 		});
 		
-		visit[0] = 0;
+		answer[0] = 0;
 		pq.offer(new Node(0,0));
 		
 		while(!pq.isEmpty()) {
@@ -87,22 +81,22 @@ public class Networking9694 {
 			int cur = nd.o;
 			int curW = -nd.w;
 			
-			if (visit[cur] < curW) continue;
+			if (answer[cur] < curW) continue;
 			
 			for (int i = 0; i < map.get(cur).size(); i++) {
 				int next =  map.get(cur).get(i).o;
 				int nextW =  map.get(cur).get(i).w;
 				
-				if (visit[next] > curW + nextW) {
-					visit[next] = curW + nextW;
-					pq.offer(new Node(next,-visit[next]));
+				if (answer[next] > curW + nextW) {
+					answer[next] = curW + nextW;
+					pq.offer(new Node(next,-answer[next]));
 					prev[next] = cur;
 				}
 			}
 		}//while
 		
 		
-		if (visit[M-1] == INF) System.out.printf("Case #%d: -1\n",CASE++);
+		if (answer[M-1] == INF) System.out.printf("Case #%d: -1\n",CASE++);
 		else {
 			Stack<Integer> stack = new Stack<>();
 			
@@ -124,7 +118,7 @@ public class Networking9694 {
 	
 	static void clean() {
 		for (int i = 0; i < M; i++) {
-			visit[i] = INF;
+			answer[i] = INF;
 			prev[i] = -1;
 		}
 	}
--- Java/com.test.algorithm/src/com/test/algorithm/TestTestTest.java
+++ Java/com.test.algorithm/src/com/test/algorithm/TestTestTest.java
@@ -4,108 +4,50 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-public class TestTestTest {
 
+public class TestTestTest {
 	
-	public static class Vertex implements Comparable<Vertex>{
-		int no;
-		int w;
+	
+	static class Node {
+		int v, w;
 		
-		Vertex(int no, int w){
-			this.no = no;
+		public Node(int v, int w) {
+			this.v = v;
 			this.w = w;
 		}
-
-		@Override
-		public int compareTo(Vertex o) {
-			return this.w - o.w;
-		}
 	}
 	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws Exception{
 		
-		for(int tc=1;tc<=T;tc++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());
-			int M = Integer.parseInt(st.nextToken());
-			
-			ArrayList<ArrayList<Vertex>> list = new ArrayList<>();
-			for(int i=0;i<M;i++) {
-				list.add(new ArrayList<>());
-			}
-			
-			for(int i=0;i<N;i++) {
-				st = new StringTokenizer(br.readLine());
-				int x = Integer.parseInt(st.nextToken());
-				int y = Integer.parseInt(st.nextToken());
-				int z = Integer.parseInt(st.nextToken());
+		
+		PriorityQueue<Node> pq = new PriorityQueue<>(new Comparator<Node>() {
+			@Override
+			public int compare(Node n1, Node n2) {
 				
-				list.get(x).add(new Vertex(y,z));
-				list.get(y).add(new Vertex(x,z));
+				if (n1.w > n2.w) return 1;
+				else if (n2.w > n1.w) return -1;
+				else return 0;
 			}
-			
-			int[][] distance = new int[M][2];
-			boolean[] visited = new boolean[M];
-			
-			for(int i=0;i<M;i++) {
-				distance[i] = new int[] {Integer.MAX_VALUE, -1};
-			}
-			
-			int start = 0;
-			distance[start][0] = 0;
-			
-			PriorityQueue<Vertex> pq = new PriorityQueue<>();
-			pq.offer(new Vertex(start,distance[start][0]));
-			
-			Stack<Integer> answer = new Stack<>();
-			
-			while(!pq.isEmpty()) {
-				int current = pq.poll().no;
-				
-				if(visited[current]) continue;
-				visited[current] = true;
-				
-				if(current == M-1) {
-					while(current != -1) {
-						answer.push(current);
-						current = distance[current][1];
-					}
-					break;
-				}
-				
-				for(Vertex v : list.get(current)) {
-					if(distance[v.no][0] > distance[current][0] + v.w) {
-						distance[v.no][0] = distance[current][0] + v.w;
-						distance[v.no][1] = current;
-						pq.offer(new Vertex(v.no,distance[v.no][0]));
-					}
-				}
-			}
-			
-			
-			sb.append("Case #"+tc+": ");
-			if(answer.size() > 0) {
-				while(!answer.isEmpty()) sb.append(answer.pop() + " ");
-				
-				sb.setLength(sb.length()-1);
-				
-			}
-			
-			else {
-				sb.append(-1);
-			}
-			sb.append("\n");
-			
-			
-		}
-		System.out.print(sb);
+		});
+	
+		pq.offer(new Node(1,31));
+		pq.offer(new Node(2,43));
+		pq.offer(new Node(3,-66));
+		
+		System.out.println(pq.peek().v + " " + pq.peek().w);
+	
 	}
+	
+	
+	
+	
+	
+	
 
 }
