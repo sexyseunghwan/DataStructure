2501380a5997c16dbafdb3f94bfc143a095e55ff
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2022-05-01 22:55:23 +0900

    2022-05-01
    2022-05-01

---
+++ Java/com.test.algorithm/src/com/test/algorithm/Hacking10282.java
@@ -0,0 +1,22 @@
+package com.test.algorithm;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Hacking10282 {
+	
+	static class Node {
+		//int 
+	}
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		
+		
+	}
+
+}
