ba902efafbae24b261616d3f9825e1b62b47a594
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2022-09-25 22:22:11 +0900

    2022-09-25
    2022-09-25

--- Java/com.test.algorithm/src/com/test/algorithm/Sumdivide2225_1.java
+++ Java/com.test.algorithm/src/com/test/algorithm/Sumdivide2225_1.java
@@ -1,10 +1,7 @@
 package com.test.algorithm;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Sumdivide2225_1 {
 	
@@ -23,20 +20,13 @@ public class Sumdivide2225_1 {
 		
 		mat = new long[K+1][N+1];
 		
-		for (int i = 0; i <= K; i++) mat[i][0] = 1;
-		for (int i = 0; i <= N; i++) mat[1][i] = 1;
 		
 		
-		for (int i = 2; i <= K; i++) {
+		for (int i = 1; i <= K; i++) {
 			for (int j = 1; j <= N; j++) {
+				for (int k = 0; k <= j; k++) mat[i][j] += mat[i-1][k]; 
 				
-				long sumVal = 0;
-				
-				for (int k = 0; k <= j; k++) {
-					sumVal += mat[i-1][k];
-				}
-				
-				mat[i][j] = sumVal % DIV;
+				mat[i][j] %= DIV;
  			}
 		}
 		
@@ -45,12 +35,5 @@ public class Sumdivide2225_1 {
 		bw.close();
 		br.close();
 		
-	}
-	
-	static int dp(int r, int c) {
-		
-		
-		
-		return 0;
 	}
 }
